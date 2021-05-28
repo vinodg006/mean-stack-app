07f3ae8793440ebaf74d3ced8b5ad7f45585374e
Vinod Gupta <VinodG@MITR-2MG1HW.local>
2021-05-28 14:45:45 +0530

    WIP

---
+++ config/database.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // configure the code below with your username, password and mlab database information
+  database:
+    "mongodb+srv://vinod:gKe0rQwGgdVtv4Xu@cluster0.lzzbr.mongodb.net/meanauthapp?retryWrites=true&w=majority", //prod
+  //database: 'mongodb://localhost:27017/meanauth',    //dev
+  secret: "mysecret123",
+};
